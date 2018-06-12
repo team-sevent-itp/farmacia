b1097fbf4602a73fdd88291cb82c663bbe04cf98
nuevo <nuevo@DESKTOP-DDK0R7E>
2018-06-12 02:48:52 -0500

    DAO PARA PAGINACION DEFINIDO

--- src/main/java/com/core/farmacia/App.java
+++ src/main/java/com/core/farmacia/App.java
@@ -2,7 +2,10 @@ package com.core.farmacia;
 
 import com.core.farmacia.dao.DAOException;
 import com.core.farmacia.dao.DAOManager;
+import com.core.farmacia.model.Laboratorio;
 import com.core.farmacia.model.LineaProducto;
+import com.core.farmacia.model.Presentacion;
+import com.core.farmacia.model.Producto;
 import com.core.farmacia.mysql.ManagerDaoImpl;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -15,14 +18,17 @@ import java.util.List;
 public class App {
 
     public static void main(String[] args) throws DAOException {
-        
+
         LineaProducto tipo = new LineaProducto();
         tipo.setId(4);
         tipo.setNombre("jabon--N");
-        
+
         List<LineaProducto> tipoList = new ArrayList<>();
-        
-        
+        List<Producto> productList = new ArrayList<>();
+
+        Laboratorio lab = new Laboratorio();
+        Presentacion pre = new Presentacion();
+
         try {
             DAOManager manager = new ManagerDaoImpl("192.168.1.10:3306", "farmacia_popular_d", "kelium", "kelium");
             //manager.crearLineaProducto().ingresar(tipo);
@@ -30,13 +36,24 @@ public class App {
             //manager.crearLineaProducto().eliminar(tipo);
             //tipo = manager.crearLineaProducto().getOne(1L);
             //System.out.println(tipo.toString());
-            tipoList = manager.crearLineaProducto().getAll();
+            /*tipoList = manager.crearLineaProducto().getAll();
             tipoList.stream().forEach(ob -> {
+                System.out.println(ob.toString());
+            });*/
+
+            lab = manager.crearLaboratorio().getOne(2L);
+            pre = manager.crearPresentacion().getOne(2L);
+
+            System.out.println(lab.toString());
+            System.out.println(pre.toString());
+
+            productList = manager.crearProducto().getPaginacion(1,1000);
+            productList.stream().forEach(ob -> {
                 System.out.println(ob.toString());
             });
         } catch (SQLException ex) {
             new DAOException("Error al iniciar la conexion", ex);
-        }        
+        }
 
     }
 
